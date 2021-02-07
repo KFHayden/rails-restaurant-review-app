e2b95cb956966d59a8aaa7ba6e8c64c0bcb722df
KFHayden <kevin.fhayden@gmail.com>
2021-02-06 21:23:17 -0600

    add login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find_by_id(params[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,10 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(username: params[:user][:username])
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
+
+        redirect_to user_path(user)
     end
 
     def destroy
