ecd95401eaa926c1d952f0f116546d03bfe108c2
KFHayden <kevin.fhayden@gmail.com>
2021-02-09 14:15:49 -0600

    removed edit functionality for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,6 @@ class UsersController < ApplicationController
         @user = User.find_by_id(params[:id])
     end
 
-    def edit
-        @user = User.find_by_id(params[:id])
-    end
-
     private
 
     def user_params
