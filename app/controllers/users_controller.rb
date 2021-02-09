9d64bcbde4def3777207079640c1b4fe5a53f96f
KFHayden <kevin.fhayden@gmail.com>
2021-02-09 14:00:32 -0600

    add form partial to user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,10 @@ class UsersController < ApplicationController
         @user = User.find_by_id(params[:id])
     end
 
+    def edit
+        @user = User.find_by_id(params[:id])
+    end
+
     private
 
     def user_params
