9814c7b973568b3018df4fd9d8592ee447eb4588
KFHayden <kevin.fhayden@gmail.com>
2021-02-09 14:18:51 -0600

    add show/create functionality to reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,7 @@ class ReviewsController < ApplicationController
     end
 
     def show
+        @review = Review.find_by_id(params[:id])
     end 
 
     def new
@@ -12,6 +13,8 @@ class ReviewsController < ApplicationController
     end 
 
     def create
+        @review = Review.create(review_params)
+        redirect_to review_path
     end
 
     def edit
